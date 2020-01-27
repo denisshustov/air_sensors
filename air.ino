872680f48c411510ed888e47998859e2215d3d10
Denis Shustov <denis.shustov.1984@gmail.com>
2020-01-27 18:35:04 +0200

    Update air.ino

--- air.ino
+++ air.ino
@@ -3,6 +3,14 @@
 #include <SoftwareSerial.h>
 #include <TimeLib.h>
 #include <DS1307RTC.h>
+#include <SPI.h>
+#include <SD.h>
+#include <OneWire.h>
+
+OneWire ds(6);
+
+const int chipSelect =7;
+
 
 const int analogInPin0 = A0;
 const int analogInPin1 = A1; 
@@ -14,11 +22,12 @@ int sensorValue2 = 0;
 
 
 MS5611 ms5611;
-double referencePressure;
+//double referencePressure;
 
 
 byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
 unsigned char response[9];
+int error = 0;//1 - card, 2 - barometer, 3 - time SetTime, 4 - time circuitry, 5 - CO2, 6 - open log file
 
 
 void setup() {
@@ -29,55 +38,66 @@ void setup() {
   
   Serial.println("Serial ok");
   Serial1.println("Serial1 ok");
+   
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    error = 1;
+    showAlarm();
+  }
 
   while(!ms5611.begin())
   {
-    Serial.println("Could not find a valid MS5611 sensor, check wiring!");
-    delay(500);
+    //Serial.println("Could not find a valid MS5611 sensor, check wiring!");
+    error = 2;
+    showAlarm();
+    //delay(500);
   }
-  referencePressure = ms5611.readPressure();
-  checkSettings();
-}
-void checkSettings()
-{
-  Serial.print("Oversampling: ");
-  Serial.println(ms5611.getOversampling());
+  //referencePressure = ms5611.readPressure();
+  //ms5611.getOversampling();
 }
 
-  
+void showAlarm() {
+
+  while (1);
+}
+
 void loop() {
+  String dataString = "";//TIME;MQ2;MQ135;MQ7;pressue;temp1;CO2;temp2
+
+  //---------------------TIME---------------------------
+
+  tmElements_t tm;
+  if (RTC.read(tm)) {
+    dataString = String(tm.Hour) + ":" + String(tm.Minute) + ":" + String(tm.Second) + " " +
+     String(tm.Day) + "/" + String(tm.Month) + "/" + String(tmYearToCalendar(tm.Year)) + ";";
+  } else {
+    
+    if (RTC.chipPresent()) {
+      //Serial.println("The DS1307 is stopped.  Please run the SetTime");
+      error = 3;
+    } else {
+      error = 4;
+      //Serial.println("DS1307 read error!  Please check the circuitry.");
+    }
+    showAlarm();
+  }
+//---------------------TIME---------------------------
+
+
+
   //---------------------MQ---------------------------
 
-  sensorValue0 = analogRead(analogInPin0); //считываем значения с датчика
-  sensorValue1 = analogRead(analogInPin1); //считываем значения с датчика
-  sensorValue2 = analogRead(analogInPin2); //считываем значения с датчика
-  
-  Serial.print("MQ2= ");
-  Serial.println(sensorValue0);
-  Serial.print("MQ135= ");
-  Serial.println(sensorValue1);
-  Serial.print("CO MQ7= ");
-  Serial.println(sensorValue2);
-  Serial.println("---------------------------");
+  sensorValue0 = analogRead(analogInPin0);
+  sensorValue1 = analogRead(analogInPin1);
+  sensorValue2 = analogRead(analogInPin2);
+  dataString += String(sensorValue0) + ";" +String(sensorValue1) + ";" +String(sensorValue2) + ";";
   //---------------------MQ---------------------------
 //---------------------pressue---------------------------
   // Read true temperature & Pressure
   double realTemperature = ms5611.readTemperature();
   long realPressure = ms5611.readPressure();
 
-  Serial.println("--");
-
-  Serial.print("realTemp = ");
-  Serial.print(realTemperature);
-  Serial.println(" *C");
-
-  Serial.print("realPressure = ");
-  Serial.print(realPressure);
-  Serial.println(" Pa");
-  
-  Serial.print("realPressure = ");
-  Serial.print(realPressure/133);
-  Serial.println(" mmHg Art.");
+  dataString += String(realPressure/133) + ";" + String(realTemperature) + ";";
   
 //---------------------pressue---------------------------
 //---------------------CO2---------------------------
@@ -91,52 +111,57 @@ void loop() {
   crc++;
  
   if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
-    Serial.println("CRC error: " + String(crc) + " / "+ String(response[8]));
-    Serial.println("Sensor CRC error");
+    error = 5;
+    showAlarm();
+    //Serial.println("CRC error: " + String(crc) + " / "+ String(response[8]));
+    //Serial.println("Sensor CRC error");
   } else {
     unsigned int responseHigh = (unsigned int) response[2];
     unsigned int responseLow = (unsigned int) response[3];
     unsigned int ppm = (256*responseHigh) + responseLow;
-    Serial.println("CO2: " + String(ppm) + " ppm"); 
+    //Serial.println("CO2: " + String(ppm) + " ppm");
+    dataString += String(ppm) + ";";
   }
 //---------------------CO2---------------------------
 
-//---------------------TIME---------------------------
+//---------------------TEMP2---------------------------
+  byte data[2]; // Место для значения температуры
+  
+  ds.reset(); // Начинаем взаимодействие со сброса всех предыдущих команд и параметров
+  ds.write(0xCC); // Даем датчику DS18b20 команду пропустить поиск по адресу. В нашем случае только одно устрйоство 
+  ds.write(0x44); // Даем датчику DS18b20 команду измерить температуру. Само значение температуры мы еще не получаем - датчик его положит во внутреннюю память
+  
+  //delay(1000); // Микросхема измеряет температуру, а мы ждем.  
+  
+  ds.reset(); // Теперь готовимся получить значение измеренной температуры
+  ds.write(0xCC); 
+  ds.write(0xBE); // Просим передать нам значение регистров со значением температуры
+  // Получаем и считываем ответ
+  data[0] = ds.read(); // Читаем младший байт значения температуры
+  data[1] = ds.read();
+  float temperature =  ((data[1] << 8) | data[0]) * 0.0625;  
+  dataString += String(temperature) + ";";
 
-  tmElements_t tm;
-  if (RTC.read(tm)) {
-    Serial.print("Ok, Time = ");
-    print2digits(tm.Hour);
-    Serial.write(':');
-    print2digits(tm.Minute);
-    Serial.write(':');
-    print2digits(tm.Second);
-    Serial.print(", Date (D/M/Y) = ");
-    Serial.print(tm.Day);
-    Serial.write('/');
-    Serial.print(tm.Month);
-    Serial.write('/');
-    Serial.print(tmYearToCalendar(tm.Year));
-    Serial.println();
-  } else {
-    if (RTC.chipPresent()) {
-      Serial.println("The DS1307 is stopped.  Please run the SetTime");
-      Serial.println("example to initialize the time and begin running.");
-      Serial.println();
-    } else {
-      Serial.println("DS1307 read error!  Please check the circuitry.");
-      Serial.println();
-    }
-    delay(9000);
+
+//---------------------TEMP2---------------------------
+
+//---------------------CD CARD---------------------------
+  
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+
+    Serial.println(dataString);
   }
-//---------------------TIME---------------------------
-  delay(1000);
-}
-
-
-void print2digits(int number) {
-  if (number >= 0 && number < 10) {
-    Serial.write('0');
+  else {
+    error = 6;
+    showAlarm();
+    Serial.println("error opening datalog.txt");
   }
-  Serial.print(number);
+
+//---------------------CD CARD---------------------------
+  Serial.println(dataString);
+  delay(10000);
 }
