cbf3532f01766aaed0c20cda83a0422ae2b87ab6
Denis Shustov <denis.shustov.1984@gmail.com>
2020-05-06 23:10:36 +0300

    Update ddd.py

--- ddd.py
+++ ddd.py
@@ -9,6 +9,11 @@ import os
 import glob 
 #https://www.circuitbasics.com/raspberry-pi-ds18b20-temperature-sensor-tutorial/
 
+
+import MySQLdb
+
+
+
 #-----------------MS5611-----------------
 sensorMS5611 = MS5611(1, 0x77, 432.8)
 sensorMS5611.setElevationFt(1420)
@@ -41,15 +46,59 @@ def read_temp():
         return temp_c
 #-----------------ds18b20-----------------
 
+conn = MySQLdb.connect(host= "localhost",
+                  user="pi_user",
+                  passwd="pi_userP@$",
+                  db="measure")
+
+def write_to_db(data):
+	x = conn.cursor()
+	try:
+	   q_insert = '\
+		INSERT INTO data \
+		(ms5611_temp, \
+	     	ms5611_pressue, \
+	     	mhz19_co2, \
+	     	mhz19_temp, \
+	     	ds18b20_temp) \
+		VALUES \
+		(%.5f,%.5f,%.5f,%.5f,%.5f) \
+		' % (data['ms5611_temp'], data['ms5611_pressue'], data['mhz19_co2'], data['mhz19_temp'], data['ds18b20_temp'])
+	   print q_insert
+	   x.execute(q_insert)
+	   conn.commit()
+ 	except MySQLdb.Error as error:
+	    print "ERROR!!!!:"
+	    print(x._last_executed)
+	    print(error)
+	    conn.rollback()
+
+	finally:
+            x.close()
+            conn.close()
+	
 
 while True:
     sensorMS5611.read()
     tempC = sensorMS5611.getTempC()
-    press = sensorMS5611.getPressureAdj()
-    print tempC, "C", press*0.75006375541921, "mmHg"
-    print  mh_z19.read_all()
-    print "temperature - ", read_temp()
-    time.sleep(5)
+    press = sensorMS5611.getPressureAdj()*0.75006375541921
+    #print tempC, "C", press, "mmHg"
+
+    mhz19 = mh_z19.read_all()
+    #print mhz19['co2']
+    #print mhz19['temperature']
+    #print "temperature - ", read_temp()
+    temp = read_temp()
+    write_to_db({ \
+	'ms5611_temp': tempC, \
+	'ms5611_pressue': press, \
+	'mhz19_co2': mhz19['co2'], \
+	'mhz19_temp': mhz19['temperature'], \
+	'ds18b20_temp': temp })
+
+    time.sleep(15)
+
+
 
 
 
