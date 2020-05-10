977d3fb67196bc67681b964b051e12142d016cfa
denis.shustov.1984@gmail.com <denis.shustov.1984@gmail.com>
2020-05-10 17:21:41 +0300

    modified:   .vs/measure_site/v16/.suo 	modified:   .vs/slnx.sqlite 	modified:   Core/obj/Release/netstandard2.0/Core.csprojAssemblyReference.cache 	modified:   measure_site.sln 	new file:   measure_site/Context/ApplicationContext.cs 	new file:   measure_site/Entities/Data.cs 	modified:   measure_site/measure_site.csproj

---
+++ measure_site/measure_site/Context/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Data> Data { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;UserId=root;Password=password;database=usersdb3;");
+        }
+    }
+}
