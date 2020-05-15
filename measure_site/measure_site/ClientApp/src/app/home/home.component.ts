d2f7d583f0e0035b4c8e2abfe2aa02d1d85f1a02
denis.shustov.1984@gmail.com <denis.shustov.1984@gmail.com>
2020-05-15 20:47:25 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/home/home.component.ts 	modified:   ../../.vs/measure_site/v16/.suo 	modified:   ../Controllers/DataController.cs

--- measure_site/measure_site/ClientApp/src/app/home/home.component.ts
+++ measure_site/measure_site/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,9 @@ import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment';
+import 'lodash';
+
+declare var _: any;
 
 @Component({
   selector: 'app-home',
@@ -14,20 +17,19 @@ export class HomeComponent {
 
   lineChartLabels: Label[];// = ['January', 'February', 'March', 'April', 'May', 'June'];
   lineChartData: ChartDataSets[] = [
-    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+    { data: [], label: 'Crude oil prices' },
   ];
 
   ngOnInit() {
     this.http.get('https://localhost:5001/api/data/data').subscribe(data => {
-      var z = data;
-      this.lineChartLabels = data.map((x) => moment(x.time_stamp).format('MM/DD/YYYY HH:mm'));
-      this.lineChartData.data = data.map((x) => x.ds18b20_temp)
+      this.lineChartLabels = _.map(data, (x) => moment(x.time_stamp).format('MM/DD/YYYY HH:mm'));
+      this.lineChartData[0] = <ChartDataSets>{
+        data: _.map(data, (x) => x.ds18b20_temp),
+        label: 'Crude oil prices'
+      };
     });
+
   }
-
-  
-
-  
 
   lineChartOptions = {
     responsive: true,
